31f401df801c91c79660ace4bbdb42be04650587
Juan Ospina <juanospina11@gmail.com>
2023-02-06 14:03:49 -0500

    adds tailwind css & creates the banner & content component

---
+++ sextant/src/components/Banner.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const Banner = () => {
+  const [hideBanner, setHideBanner] = useState(false);
+
+  return (
+    <div className={`bg-indigo-600 ${hideBanner ? "hidden" : ""}`}>
+      <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap items-center justify-between">
+          <div className="flex w-0 flex-1 items-center">
+            <p className="ml-3 truncate font-medium text-white">
+              <span className="md:inline">Welcome to Sextant!</span>
+            </p>
+          </div>
+
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              onClick={() => setHideBanner(true)}
+            >
+              <svg
+                className="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
